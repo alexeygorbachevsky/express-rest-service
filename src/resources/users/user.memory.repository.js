a20c93e45814bd70cb3762b4077dc36e7346caad
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2021-05-29 15:08:32 +0300

    docs: add user repository to jsdoc

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -10,8 +10,25 @@ const Errors = require('../../errors/constants');
 
 const TABLE_NAME = 'Users';
 
+/**
+ * @module User Repository
+ */
+
+/**
+ * @import {UserInstance} from './user.model.js'
+ */
+
+/**
+ * Get all users from db. Returns the promise that resolves with array of all users
+ * @returns {Promise<UserInstance[]>} - Array of all users
+ */
 const getAll = async () => getAllEntities(TABLE_NAME);
 
+/**
+ * Get user by id from db. Returns the promise that resolves with user object
+ * @param {string} id - User Id
+ * @returns {Promise<UserInstance>} - User object
+ */
 const get = async (id) => {
   const user = await getEntity(TABLE_NAME, id);
   if (!user) {
@@ -20,6 +37,11 @@ const get = async (id) => {
   return user;
 };
 
+/**
+ * Register (add to db) new user. Returns the promise that resolves with user object
+ * @param {UserInstance} user - User object
+ * @returns {Promise<UserInstance>} - User object
+ */
 const post = async (user) => {
   const newUser = await saveEntity(TABLE_NAME, user);
   if (!newUser) {
@@ -28,6 +50,11 @@ const post = async (user) => {
   return newUser;
 };
 
+/**
+ * Remove existed user from db. Returns the promise that resolves with removed user object
+ * @param {string} id - User ID
+ * @returns {Promise<UserInstance>} - User object that was removed
+ */
 const remove = async (id) => {
   const user = await removeEntity(TABLE_NAME, id);
   if (!user) {
@@ -39,6 +66,12 @@ const remove = async (id) => {
   return user;
 };
 
+/**
+ *  Change user info in db. Returns the promise that resolves with updated user object
+ * @param {string} id - User ID
+ * @param {Object} newData - User Data that will be changed
+ * @returns {Promise<UserInstance>} - User object that was updated
+ */
 const put = async (id, newData) => {
   const user = await updateEntity(TABLE_NAME, id, newData);
   if (!user) {
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -9,34 +9,34 @@ const usersRepo = require('./user.memory.repository');
  */
 
 /**
- * Returns the promise that resolves with array of all users
+ * Get all users. Returns the promise that resolves with array of all users
  * @returns {Promise<UserInstance[]>} - Array of all users
  */
 const getAll = () => usersRepo.getAll();
 
 /**
- * Returns the promise that resolves with user object
+ * Get user by id. Returns the promise that resolves with user object
  * @param {string} id - User Id
  * @returns {Promise<UserInstance>} - User object
  */
 const get = (id) => usersRepo.get(id);
 
 /**
- * Returns the promise that resolves with user object
+ * Register new user. Returns the promise that resolves with user object
  * @param {UserInstance} user - User object
  * @returns {Promise<UserInstance>} - User object
  */
 const post = (user) => usersRepo.post(user);
 
 /**
- * Returns the promise that resolves with removed user object
+ * Remove existed user. Returns the promise that resolves with removed user object
  * @param {string} id - User ID
  * @returns {Promise<UserInstance>} - User object that was removed
  */
 const remove = (id) => usersRepo.remove(id);
 
 /**
- * Returns the promise that resolves with updated user object
+ * Change user info. Returns the promise that resolves with updated user object
  * @param {string} id - User ID
  * @param {Object} newData - User Data that will be changed
  * @returns {Promise<UserInstance>} - User object that was updated
