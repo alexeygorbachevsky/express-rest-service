1bc35190bae94200a1f979bced964a9020d53e87
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2021-05-29 14:43:30 +0300

    feat: add jsdoc for user model and user service functions

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,11 +1,31 @@
 const uuid = require('uuid');
 
+/**
+ * User object
+ * @typedef {Object} UserInstance
+ * @property {string} id - User Id
+ * @property {string} name - User Name
+ * @property {string} login - User Login
+ * @property {string} password - User Password
+ */
+
+/**
+ * Creates a new User.
+ * @class
+ */
 class User {
+  /**
+   * User class constructor
+   * @param {string} id - User Id
+   * @param {string} name - User Name
+   * @param {string} login - User Login
+   * @param {string} password - User Password
+   */
   constructor({
     id = uuid.v1(),
     name = 'USER',
     login = 'user',
-    password = 'P@55w0rd'
+    password = 'P@55w0rd',
   } = {}) {
     this.id = id;
     this.name = name;
@@ -13,11 +33,21 @@ class User {
     this.password = password;
   }
 
+  /**
+   * @static
+   * @param  {UserInstance} user - User object
+   * @return {Object} - Object with all user properties except password
+   */
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
   }
 
+  /**
+   * @static
+   * @param  {Object} body - Body properties
+   * @return {UserInstance} - User object
+   */
   static fromRequest(body) {
     return new User(body);
   }
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,13 +1,46 @@
 const usersRepo = require('./user.memory.repository');
 
+/**
+ * @module User Service
+ */
+
+/**
+ * @import {UserInstance} from './user.model.js'
+ */
+
+/**
+ * Returns the promise that resolves with array of all users
+ * @returns {Promise<UserInstance[]>} - Array of all users
+ */
 const getAll = () => usersRepo.getAll();
 
-const get = id => usersRepo.get(id);
+/**
+ * Returns the promise that resolves with user object
+ * @param {string} id - User Id
+ * @returns {Promise<UserInstance>} - User object
+ */
+const get = (id) => usersRepo.get(id);
 
-const post = user => usersRepo.post(user);
+/**
+ * Returns the promise that resolves with user object
+ * @param {UserInstance} user - User object
+ * @returns {Promise<UserInstance>} - User object
+ */
+const post = (user) => usersRepo.post(user);
 
-const remove = id => usersRepo.remove(id);
+/**
+ * Returns the promise that resolves with removed user object
+ * @param {string} id - User ID
+ * @returns {Promise<UserInstance>} - User object that was removed
+ */
+const remove = (id) => usersRepo.remove(id);
 
+/**
+ * Returns the promise that resolves with updated user object
+ * @param {string} id - User ID
+ * @param {Object} newData - User Data that will be changed
+ * @returns {Promise<UserInstance>} - User object that was updated
+ */
 const put = (id, newData) => usersRepo.put(id, newData);
 
 module.exports = { getAll, get, post, remove, put };
