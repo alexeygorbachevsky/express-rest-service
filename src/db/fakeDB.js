59ff6902e34e64abf7a3dcc41fa749b466dc00b4
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2021-05-29 17:20:08 +0300

    docs: add fakeDB to jsdoc

--- src/db/fakeDB.js
+++ src/db/fakeDB.js
@@ -2,10 +2,39 @@ const User = require('../resources/users/user.model');
 const Board = require('../resources/boards/board.model');
 const Task = require('../resources/tasks/task.model');
 
+/**
+ * @module Memory DB
+ */
+
+/**
+ * @import {UserInstance} from '../resources/user.model.js'
+ */
+
+/**
+ * Normalize db structure when users or boards deleted
+ * @typedef {function} Normalize
+ * @param {Object} removingEntity - entity that was removed
+ * @param {string} tableName - tableName of entity that was removed
+ * @returns {void}
+ */
+
+/**
+ * Memory DB
+ * @typedef {Object} fakeDB
+ * @property {UserInstance[]} Users - Array of users
+ * @property {Array}  Boards - Array of boards
+ * @property {Array}  Tasks - Array of tasks
+ * @property {Normalize}  normalize - function that normalizes db structure
+ */
+
+/**
+ * @type {fakeDB}
+ */
 const db = {
   Users: [],
   Boards: [],
   Tasks: [],
+  /** @type {Normalize} */
   normalize(removingEntity, tableName) {
     switch (tableName) {
       case 'Users': {
@@ -22,19 +51,42 @@ const db = {
         break;
       }
     }
-  }
+  },
 };
 
+/**
+ * Get all entities from specified db table. Returns the array of all found entities
+ * @param {string} tableName - Table name of target db table from where need to return entities
+ * @returns {Array<Object>} - Array of all entities
+ */
 const getAllEntities = (tableName) => db[tableName];
 
+/**
+ * Get entity by id from db
+ * @param {string} tableName - Name of table from where need to return specified entity
+ * @param {string} id - Entity Id
+ * @returns {Object} - Entity object
+ */
 const getEntity = (tableName, id) =>
   db[tableName].find((entity) => id === entity.id);
 
+/**
+ * Save entity to db
+ * @param {string} tableName - Name of table where need to save specified entity
+ * @param {Object} entity - Entity data that need to save
+ * @returns {Object} - Saved entity object
+ */
 const saveEntity = (tableName, entity) => {
   db[tableName].push(entity);
   return getEntity(tableName, entity.id);
 };
 
+/**
+ * Remove entity from db
+ * @param {string} tableName - Name of table from where need to remove specified entity
+ * @param {string} entityId - Id of removing entity
+ * @returns {Object} - Removed entity
+ */
 const removeEntity = (tableName, entityId) => {
   const removingEntity = getEntity(tableName, entityId);
   if (removingEntity) {
@@ -44,19 +96,27 @@ const removeEntity = (tableName, entityId) => {
   return removingEntity;
 };
 
-
+/**
+ * Update entity in db
+ * @param {string} tableName - Name of table from where need to remove specified entity
+ * @param {string} updatingEntityId - Id of updating entity
+ * @param {Object} newData - New data that should be pasted instead of old data
+ * @returns {Object} - Updated entity
+ */
 const updateEntity = (tableName, updatingEntityId, newData) => {
   const entityId = db[tableName].findIndex(({ id }) => id === updatingEntityId);
   if (entityId !== -1) {
     db[tableName][entityId] = {
       ...db[tableName][entityId],
-      ...newData
+      ...newData,
     };
   }
   return getEntity(tableName, updatingEntityId);
 };
 
-// Init db filling
+/**
+ * Initial db filling (IIFE). Populates the database with initial data
+ */
 (() => {
   Array(3)
     .fill(null)
