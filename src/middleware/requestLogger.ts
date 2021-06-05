163fdd74548cb713cafc99bd9ac9e8875ad0e060
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2021-06-05 15:52:57 +0300

    feat: add request logging to file

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -1,28 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
 import { finished } from 'stream';
 
+const writeToFile = require('../errors/writeToFile');
+
 const requestLogger = (req: Request, res: Response, next: NextFunction) => {
   const { url = '/', method, query = {}, body = {} } = req;
   const start = Date.now();
+  const eventTime = new Date();
   next();
 
   finished(res, (error) => {
     const ms = Date.now() - start;
     const { statusCode } = res;
+    const info = ` Request Logging:
+        Event time: ${eventTime},
+        Method: ${method}, 
+        Whole url: ${url}, 
+        Query params: ${JSON.stringify(query)}, 
+        Body: ${JSON.stringify(body)}, 
+        Status code: ${statusCode}, 
+        Milliseconds: ${ms}ms `;
 
     if (error) {
       // eslint-disable-next-line no-console
       console.error('Stream failed', error);
     } else {
       // eslint-disable-next-line no-console
-      console.log(`
-        Method: ${method}, 
-        Whole url: ${url}, 
-        Query params: ${JSON.stringify(query)}, 
-        Body: ${JSON.stringify(body)}, 
-        Status code: ${statusCode}, 
-        Milliseconds: ${ms}ms `);
+      console.log(info);
     }
+    writeToFile(info, './logging.log');
   });
 };
 
